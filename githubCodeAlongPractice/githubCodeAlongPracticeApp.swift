938191e81aa05aa3f44dead7ba0b09fe60125d07
Paige <makipaige11@gmail.com>
2023-08-15 11:52:23 -0500

    Initial Commit

---
+++ githubCodeAlongPractice/githubCodeAlongPracticeApp.swift
@@ -0,0 +1,17 @@
+//
+//  githubCodeAlongPracticeApp.swift
+//  githubCodeAlongPractice
+//
+//  Created by Paige Maki on 8/15/23.
+//
+
+import SwiftUI
+
+@main
+struct githubCodeAlongPracticeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
